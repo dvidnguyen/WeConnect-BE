727eda0e6fef0a818022346d6546fe7dc0082e0a
dvidnguyen <qndjxz@gmail.com>
2025-07-26 22:46:02 +0700

    first commit

---
+++ src/main/java/com/example/WeConnect_BE/WeConnectBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.WeConnect_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeConnectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeConnectBeApplication.class, args);
+	}
+
+}
