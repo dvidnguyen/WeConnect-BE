dea566f81a823671a3b90334d9e20244335de713
Duc <nguyentrongduc447@gmail.com>
2025-08-19 22:49:14 +0700

    fix

--- src/main/java/com/example/WeConnect_BE/controller/ConversationController.java
+++ src/main/java/com/example/WeConnect_BE/controller/ConversationController.java
@@ -35,8 +35,8 @@ public class ConversationController {
     }
     //Lấy danh sách conversation (kèm lastMessage + unreadCount)
 
-    @GetMapping("/{userId}")
-    public ApiResponse<List<ConversationRow>> list(@PathVariable("userId") String userId) {
+    @GetMapping("/")
+    public ApiResponse<List<ConversationRow>> list() {
         var data = conversationService.getList();
         return ApiResponse.<List<ConversationRow>>builder()
                 .result(data)
